59d166534478feb99a5c12e7c357e0133d45a9bd
Albert Ho <albertcyi.ho@gmail.com>
2022-02-01 12:37:47 -0800

    Refactor - access element via dom traversal instead of directly via class name

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,13 +2,15 @@ $(() => {
   $("section#new-tweet > form > textarea#tweet-text").on("input", function () {
     const counter = 140 - $(this).val().length;
     const textColor = counter >= 0 ? "inherit" : "red";
-    $(".counter").text(counter).css("color", textColor);
+    // $(".counter").text(counter).css("color", textColor);
 
     // *** review - compass wants .counter
     // to be selected via dom traversal***
-    // $(this)
-    //   .parent("form")
-    //   .children("div > output.counter")
-    //   .text(140 - $(this).val().length);
+    $(this)
+      .parent()
+      .children("div")
+      .children("output.counter")
+      .text(counter)
+      .css("color", textColor);
   });
 });
