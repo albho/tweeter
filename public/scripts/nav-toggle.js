fa4e25ef503d9e2426fc2b997e57a13de025a574
Albert Ho <albertcyi.ho@gmail.com>
2022-02-02 10:18:24 -0800

    Implement button to scroll back up and show text field when user scrolls down page

--- public/scripts/nav-toggle.js
+++ public/scripts/nav-toggle.js
@@ -3,15 +3,15 @@ $(() => {
     if ($("section#new-tweet").css("display") === "none") {
       // show and focus text field, toggle button arrow direction
       $("section#new-tweet").slideDown();
+      $("textarea#tweet-text").focus();
       $("i.fa-angle-double-up").css("display", "block");
       $("i.fa-angle-double-down").css("display", "none");
-      $("textarea#tweet-text").focus();
     } else {
       // hide and blur text field, toggle button arrow direction
       $("section#new-tweet").slideUp();
+      $("textarea#tweet-text").blur();
       $("i.fa-angle-double-down").css("display", "block");
       $("i.fa-angle-double-up").css("display", "none");
-      $("textarea#tweet-text").blur();
     }
   });
 });
---
+++ public/scripts/scroll-up.js
@@ -0,0 +1,26 @@
+$(() => {
+  // scroll back up with animation
+  $("button#scroll-up").on("click", () => {
+    $("html").animate(
+      {
+        scrollTop: 0,
+      },
+      500
+    );
+
+    // show and focus text field
+    $("section#new-tweet").slideDown();
+    $("textarea#tweet-text").focus();
+    $("i.fa-angle-double-up").css("display", "block");
+    $("i.fa-angle-double-down").css("display", "none");
+  });
+
+  // toggle scroll up button based on page position
+  $(document).scroll(function () {
+    if ($(this).scrollTop() < 200) {
+      $("button#scroll-up").fadeOut();
+    } else {
+      $("button#scroll-up").fadeIn();
+    }
+  });
+});
