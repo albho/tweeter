db16f99af79d04c576be0812fef4e76466c7a1b7
Albert Ho <albertcyi.ho@gmail.com>
2022-02-02 09:49:06 -0800

    Toggle focus/blur on text field when it is toggled to show/hide

--- public/scripts/nav-toggle.js
+++ public/scripts/nav-toggle.js
@@ -1,13 +1,17 @@
 $(() => {
   $("button#nav-toggle").click(() => {
     if ($("section#new-tweet").css("display") === "none") {
+      // show and focus text field, toggle button arrow direction
       $("section#new-tweet").slideDown();
       $("i.fa-angle-double-up").css("display", "block");
       $("i.fa-angle-double-down").css("display", "none");
+      $("textarea#tweet-text").focus();
     } else {
+      // hide and blur text field, toggle button arrow direction
       $("section#new-tweet").slideUp();
       $("i.fa-angle-double-down").css("display", "block");
       $("i.fa-angle-double-up").css("display", "none");
+      $("textarea#tweet-text").blur();
     }
   });
 });
