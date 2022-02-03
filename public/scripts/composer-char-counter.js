fbda394d685180a1c6b8fecd2dcdcd4f953d3293
Albert Ho <albertcyi.ho@gmail.com>
2022-02-03 15:28:14 -0800

    Refactor - move function out of doc ready, access counter via dom traversal

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,20 @@
 $(() => {
-  $("#tweet-text").on("input", function () {
-    const counter = 140 - $(this).val().length;
-    const textColor = counter >= 0 ? "inherit" : "red";
-    $("form button").prop("disabled", false);
-
-    $(this)
-      .parent()
-      .children("div")
-      .children("#char-counter")
-      .text(counter)
-      .css("color", textColor);
-
-    $("#error-message").slideUp();
-  });
+  $("#tweet-text").on("input", onInput);
 });
+
+function onInput() {
+  const $input = $(this);
+
+  // enable submit button and ensure error message is hidden on input change
+  $("form button").prop("disabled", false);
+  $("#error-message").slideUp();
+
+  // dynamically change counter number and color
+  const $charLength = $input.val().length;
+  const counterText = 140 - $charLength;
+  const textColor = counterText >= 0 ? "inherit" : "red";
+
+  const $form = $input.closest("form");
+  const $counter = $form.find("#char-counter");
+  $counter.text(counterText).css("color", textColor);
+}
