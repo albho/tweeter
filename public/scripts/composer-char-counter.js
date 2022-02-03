6dc8969e33155a83c6f8f903e0074160da00cc44
Albert Ho <albertcyi.ho@gmail.com>
2022-02-03 13:41:55 -0800

    Error message - change color and hide on input change

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,15 +2,14 @@ $(() => {
   $("#tweet-text").on("input", function () {
     const counter = 140 - $(this).val().length;
     const textColor = counter >= 0 ? "inherit" : "red";
-    // $(".counter").text(counter).css("color", textColor);
 
-    // *** review - compass wants .counter
-    // to be selected via dom traversal***
     $(this)
       .parent()
       .children("div")
       .children("output.counter")
       .text(counter)
       .css("color", textColor);
+
+    $("#error-message").slideUp();
   });
 });
