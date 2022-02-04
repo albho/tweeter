ddbc08c79a1ec17ad19ddea362eae4a106d62e3d
Albert Ho <albertcyi.ho@gmail.com>
2022-02-03 16:01:19 -0800

    Refactor - remove disable button and error message logic, add scoping protection

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,104 +5,108 @@
  */
 
 $(() => {
-  // helper function to prevent XSS
-  const escape = function (str) {
-    let div = document.createElement("div");
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  };
-
-  // create template string with tweet obj
-  const createTweetElement = tweet => {
-    const { user, content, created_at } = tweet;
-
-    let $tweet = `
-      <article>
-        <header>
-          <div class="user-info">
-            <div>
-              <img src='${escape(user.avatars)}' alt='user avatar'>
-              <p>${escape(user.name)}</p>
-            </div>
-            <p class="handle">${escape(user.handle)}</p>
-          </div>
-          <div class="content">
-            <p>
-              ${escape(content.text)}
-            </p>
-          </div>
-        </header>
-        <footer>
-          <p>${timeago.format(created_at)}</p>
-          <div class="icons">
-            <i class="fa-solid fa-flag fa-xs"></i>
-            <i class="fa-solid fa-retweet fa-xs"></i>
-            <i class="fa-solid fa-heart fa-xs"></i>
-          </div>
-        </footer>
-      </article>
-    `;
-
-    return $tweet;
-  };
-
-  // append templated tweet to page
-  const renderTweets = tweets => {
-    // order (most recent at the top)
-    tweets.reverse();
-
-    $("#tweet-text").val("");
-    $("#char-counter").text(140);
-    $("#tweets-container").empty();
-    $("form button").prop("disabled", false);
-    $("form button").text("Tweet");
-
-    // render tweets
-    for (const tweet of tweets) {
-      const newTweet = createTweetElement(tweet);
-      $("#tweets-container").append(newTweet);
-    }
-  };
-
-  // get tweets from server
-  const loadTweets = () => {
-    $.ajax({ method: "GET", url: "/tweets" }).then(data => {
-      renderTweets(data);
-    });
-  };
-
-  const renderErrMsg = msg => {
-    $("#error-message").text(msg).slideDown(200);
-  };
-
-  // send data to server via ajax
-  $("form").submit(e => {
-    e.preventDefault();
-
-    $("#error-message").text("").slideUp(200);
-    $("form button").prop("disabled", true);
-    $("form button").text("Tweeting...");
-
-    // error handling
-    const tweetLength = $("#tweet-text").val().length;
-    if (!tweetLength) {
-      return renderErrMsg("Tweet cannot be empty.");
-    }
-
-    if (tweetLength > 140) {
-      return renderErrMsg(
-        `Tweet cannot exceed 140 characters. You currently have ${tweetLength} characters.`
-      );
-    }
-
-    // send tweet to server
-    $.ajax({
-      method: "POST",
-      url: $("form").attr("action"),
-      data: $("form").serialize(),
-      success: () => loadTweets(),
-    });
-  });
-
   loadTweets();
 });
+
+// get tweets from server
+const loadTweets = () => {
+  $.ajax({ method: "GET", url: "/tweets" }).then(data => {
+    renderTweets(data);
+  });
+};
+
+// $.get("/tweets").then(data => {
+//   $("#tweets-container").empty();
+//   $("#textarea").val("");
+//   renderTweets(data);
+// });
+
+// append templated tweet to page
+const renderTweets = tweets => {
+  $("#tweet-text").val("");
+  $("#char-counter").text(140);
+  $("#tweets-container").empty();
+  $("form button").prop("disabled", false);
+  $("form button").text("Tweet");
+
+  // render tweets
+  tweets.forEach(tweet => {
+    const newTweet = createTweetElement(tweet);
+    $("#tweets-container").prepend(newTweet);
+  });
+};
+
+// create template string with tweet obj
+const createTweetElement = tweet => {
+  const { user, content, created_at } = tweet;
+
+  let $tweet = `
+    <article>
+      <header>
+        <div class="user-info">
+          <div>
+            <img src='${escape(user.avatars)}' alt='user avatar'>
+            <p>${escape(user.name)}</p>
+          </div>
+          <p class="handle">${escape(user.handle)}</p>
+        </div>
+        <div class="content">
+          <p>
+            ${escape(content.text)}
+          </p>
+        </div>
+      </header>
+      <footer>
+        <p>${timeago.format(created_at)}</p>
+        <div class="icons">
+          <i class="fa-solid fa-flag fa-xs"></i>
+          <i class="fa-solid fa-retweet fa-xs"></i>
+          <i class="fa-solid fa-heart fa-xs"></i>
+        </div>
+      </footer>
+    </article>
+  `;
+
+  return $tweet;
+};
+
+// send data to server via ajax
+$("form").submit(e => {
+  e.preventDefault();
+
+  $("#error-message").text("").slideUp(200);
+  $("form button").prop("disabled", true);
+  $("form button").text("Tweeting...");
+
+  // error handling
+  const tweetLength = $("#tweet-text").val().length;
+  if (!tweetLength) {
+    return renderErrMsg("Tweet cannot be empty.");
+  }
+
+  if (tweetLength > 140) {
+    return renderErrMsg(
+      `Tweet cannot exceed 140 characters. You currently have ${tweetLength} characters.`
+    );
+  }
+
+  // send tweet to server
+  $.ajax({
+    method: "POST",
+    url: $("form").attr("action"),
+    data: $("form").serialize(),
+    success: () => loadTweets(),
+  });
+  // $.post("/tweets", $("form").serialize()).then(() => loadTweets());
+});
+
+// helper function to prevent XSS
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
+const renderErrMsg = msg => {
+  $("#error-message").text(msg).slideDown(200);
+};
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,18 @@
-$(() => {
-  $("#tweet-text").on("input", onInput);
-});
+(function ($) {
+  $(() => {
+    $("#tweet-text").on("input", onInput);
+  });
 
-function onInput() {
-  const $input = $(this);
+  function onInput() {
+    const $input = $(this);
 
-  // enable submit button and ensure error message is hidden on input change
-  $("form button").prop("disabled", false);
-  $("#error-message").slideUp();
+    // dynamically update counter number and color
+    const counterText = 140 - $input.val().length;
+    const textColor = counterText >= 0 ? "inherit" : "red";
 
-  // dynamically change counter number and color
-  const $charLength = $input.val().length;
-  const counterText = 140 - $charLength;
-  const textColor = counterText >= 0 ? "inherit" : "red";
+    const $form = $input.closest("form");
+    const $counter = $form.find("#char-counter");
 
-  const $form = $input.closest("form");
-  const $counter = $form.find("#char-counter");
-  $counter.text(counterText).css("color", textColor);
-}
+    $counter.text(counterText).css("color", textColor);
+  }
+})(jQuery);
